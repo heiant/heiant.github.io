9f4e40f1609fb24ec85cdb293158d52134a8e29d
heiant <hendriantosubair69@gmail.com>
2023-08-25 09:14:33 +0800

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+document.getElementById("paragraf").innerHTML = "Pertemuan ke 7"
+function ubahwarna() {
+    document.getElementById('kelas1').style.backgroundColor = 'red';
+    document.getElementById('detail').style.color = 'blue';
+
+}
+var a = document.getElementById('heading')
+a.addEventListener(mouseOver.ubahWarna)
+function ubahWarna() {
+    
+}
